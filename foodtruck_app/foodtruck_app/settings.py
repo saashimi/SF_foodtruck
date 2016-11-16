a65e244f2ac57118e0fc01a318b52188ed7de300
saashimi <saavedra.kevin@gmail.com>
2016-11-16 07:55:02 -0800

    add secrets file.

--- foodtruck_app/foodtruck_app/settings.py
+++ foodtruck_app/foodtruck_app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+from .foodtruck_secrets import SUPER_SECRET_KEY
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '0oo5us(#=f%xmta%7$ms2ex2!$_s^tyze@e$!@^a%^-g#_0j6#'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
