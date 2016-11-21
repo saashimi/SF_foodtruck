6111d036e6e08f723bae8aa1952d5efcdd699c9d
saashimi <saavedra.kevin@gmail.com>
2016-11-20 18:21:44 -0800

    working display of food trucks per hard-coded radius

--- foodtruck_app/cart_data/views.py
+++ foodtruck_app/cart_data/views.py
@@ -9,13 +9,10 @@ from .models import Cart_db_data
 class MainPageView(TemplateView): 
     template_name =  'cart_data/index.html'
 
-"""
-def cart_db_data_view(request):
-    cart_db_data_as_geojson = serialize('geojson', Cart_db_data.objects.all())
-    return HttpResponse(cart_db_data_as_geojson, content_type='json')
-"""
-
-def cart_db_data_view(request, lat, lng, radius=1609*.5):
+def cart_db_data_view(request, radius=1609*.5):
+    lat = request.GET.get('lat')
+    lng = request.GET.get('lng')
+    print(lat, lng)
     user_location = fromstr('POINT({0} {1})'.format(lng, lat))
     desired_radius = {'m' : radius}
     nearby_carts = Cart_db_data.objects.filter(
